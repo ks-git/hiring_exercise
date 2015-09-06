c49e9db7dae9a890acf0ef5050ace5132362243a
ks-git <kathula_srinivas@hotmail.com>
2015-09-06 01:31:16 -0500

    completed the exercise and checking in code changes to a fork.

--- software-engineer/src/main/java/com/drmtx/app/Application.java
+++ software-engineer/src/main/java/com/drmtx/app/Application.java
@@ -1,25 +1,29 @@
 package com.drmtx.app;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
+@EnableJpaRepositories
 @SpringBootApplication
+@ComponentScan
 @EntityScan(basePackageClasses=Application.class)
 @Configuration
 public class Application {
 
   private static final Logger _logger = LogManager.getLogger(Application.class);
+  
 
   @Bean
   protected ServletContextListener listener() {
@@ -37,8 +41,8 @@ public class Application {
 
     };
   }
-
+  
   public static void main(String[] args) throws Exception {
-    ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
